f49eceebee4e2faefd97b82cf48183e7dbc4d4dc
Adam Gonzales <adamgonzales1@gmail.com>
2016-02-27 16:26:58 -0700

    Add anchor navigation scroll animation

--- js/index.js
+++ js/index.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
 		$('.menu').slideToggle('.slide-menu');
 	});
 
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $($(this).attr('href'));
+
+		if(target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 500);
+		}
+	});
+
 });

d2e3a0d52f970fbccb3e4b08ea1ce05de296bcf0
Adam Gonzales <adamgonzales1@gmail.com>
2016-09-21 11:32:54 -0700

    add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp 					= require('gulp');
+var browserSync 	= require('browser-sync').create();
+var sass 					= require('gulp-sass');
+var autoprefixer 	= require('gulp-autoprefixer');
+
+// static server + watch scss/html files
+gulp.task('serve', ['sass'], function() {
+	browserSync.init({
+		server: "./"
+	});
+
+	gulp.watch("./sass/*.scss", ['sass']);
+	gulp.watch("./js/*.js").on('change', browserSync.reload);
+	gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// compile sass & auto-inject into browsers
 
 //sass
 gulp.task('sass', function() {
-	gulp.src('./sass/**/*.scss')
+	return gulp.src('./sass/*.scss')
 	.pipe(sass().on('error', sass.logError))
 //pipe to autoprefixer
 	.pipe(autoprefixer({
-		browsers: ['last 3 versions'],
+		browsers: ['last 5 versions'],
 		cascade: false
 	}))
-	.pipe(gulp.dest('./css'));
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.stream());
 });
 
-//watch
-gulp.task('watch', function() {
-	gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-gulp.task('default', ['watch', 'sass'])
+gulp.task('default', ['serve']);
